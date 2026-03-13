c99c619d6d52bf36e08b2f65931142c0002f7a7d
Danicico <danicicots@gmail.com>
2026-03-13 10:45:47 +0000

    esercizio 1

---
+++ esercizio_1.py
@@ -0,0 +1,69 @@
+def is_pari (n):
+    '''Vedo se un numero è pari o dispari'''
+    bool b
+    if n%2 == 0
+        return True
+    else 
+        return False
+
+def return_valore ():
+    '''Chiedo all'utente un numero positivo'''
+    a = True
+    while a:
+        b = input('scrivi un numero positivo: ')
+        if b > 0:
+            a = False
+    return b
+
+def lista_valore_pr ():
+    '''creo una lista in base al numero precedente'''
+    a = return_valore ()
+    b = 0
+    valoritr = []
+    while a >= 1 and b <= 100 :
+        if is_pari (a):
+            a = a/2
+        else :
+            a = a*3 +1
+        valoritr.append(a)
+        b += 1
+    return valoritr
+
+def analizza_sequenza (lista) :
+    '''Vedo qual'è il valore maggiore, la somma e la lunghezza della lista'''
+    massimo = 0
+    somma = 0
+    controllo = 0
+    lunghezza = len (lista)
+    while controllo < lunghezza:
+        valorel = lista [controllo]
+        if valorel > massimo:
+            massimo = valorel
+        somma = somma + valorel
+    return massimo, somma, lunghezza
+
+def ricerca_divisibili_5 (lista):
+    '''ricerco e trascrivo i numeri divisibili per 5'''
+    length = len (lista)
+    control = 0
+    ndivisibili = 0
+    print ('i numeri divisibili per 5 sono: '/n)
+    while control < length:
+        valorel = lista [control]
+        if valorel % 5 == 0:
+            print (valorel)
+            ndivisibili += 1
+        control+=1
+    if ndivisibili == 0:
+        print('non erano presenti numeri divisibili per 5')
+
+main:
+numeri_da_analizzare = input ('qunati numeri vuoi analizzare? ')
+
+for i in range (numeri_da_analizzare):
+    numero_controllo = return_valore ()
+    print (is_pari (numero controllo))
+    sequenza = lista_valore_pr ()
+    print (analizza_sequenza (sequenza))
+    ricerca_divisibili_5 (sequenza)
+

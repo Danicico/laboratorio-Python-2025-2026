47e2e4199fc77717ffae19c1bafd69fc68ddb6b9
Danicico <danicicots@gmail.com>
2026-06-11 11:00:00 +0200

    Update esercizio_5.py

--- esercizio_5.py
+++ esercizio_5.py
@@ -43,7 +43,28 @@ def soluzione_ok(soluzione_posizioni):
 
     # Se non è ritornato prima, 
     # allora nessun incrocio trvato: posizioni della soluzione valide 
-    return True 
+    return True
+
+def rotazione_90(soluzione_posizioni, dim):
+    lista_90 = []
+    for i in range(dim):
+        lista_90.append(0)
+    for c in range(dim):
+        lista_90[soluzione_posizioni[c]] = 7-c
+    return lista_90
+
+def rotazione_180(soluzione_posizioni, dim):
+    lista_180 = []
+    for i in range(dim):
+        lista_180.append(0)
+    for c in range(dim):
+        lista_180[7-c]= 7-soluzione_posizioni[c]
+    return lista_180
+
+def rotazione_270(soluzione_posizioni, dim):
+    lista_t_90 = rotazione_90(soluzione_posizioni, dim)
+    lista_270 = rotazione_180(lista_t_90, dim)
+    return lista_270
 
 
 
@@ -78,7 +99,7 @@ def main():
         while solutions < 1:
 
             controllo = True 
-    
+            lista_rotazioni = []
             # permutazione casuale della soluzione 'mescolando' posizioni
 
             random_generator.shuffle(scacchiera)
@@ -98,11 +119,15 @@ def main():
             if soluzione_ok(scacchiera) and controllo: 
                 # se la soluzione è buona, scrive
                 print(f'Found solution {scacchiera} in {time.time() - start_time} s.')
+                tempo = time.time() - start_time
+                lista_rotazioni.append(rotazione_90(scacchiera, int(n)))
+                lista_rotazioni.append(rotazione_180(scacchiera, int(n)))
+                lista_rotazioni.append(rotazione_270(scacchiera, int(n)))
             
                 # incremento contatore soluzioni trovate (condizione stop loop)
                 if controllo:
                     solutions += 1      
-                lista_tempo.append (time.time()-start_time)
+                lista_tempo.append (tempo)
                 scacchiera_1 = tuple(scacchiera)
                 lista_combinazioni.append(scacchiera_1)
                 # reset timer ricerca soluzione
@@ -111,7 +136,25 @@ def main():
                 print("il numero di tentativi per trovare " + str(solutions) + " soluzioni è circa: " + str(numero_tentativi))
                 numero_tentativi = 0
         print (dizionario_ripetizioni)
+        print(lista_rotazioni)
     else:
         print ('numero di righe non valido')
+    tempo_inizio = time.time()
+    control_numero = 2
+    control = True
+    """while control:
+        random_generator.shuffle(list(range(int(control_numero))))
+        if soluzione_ok(scacchiera) and controllo: 
+            tempo = time.time() - tempo_inizio
+        if tempo>15:
+            control = False
+        else:
+            control_numero+=1
+    print ("la dimensione minima è " + str(control_numero))"""
+
+
+
+
+
 # chiamo la funzione principale 
 main()

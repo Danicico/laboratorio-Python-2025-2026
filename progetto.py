21a7f29f42afb90f10148872a295f03a2c889678
Danicico <danicicots@gmail.com>
2026-06-18 08:56:19 +0000

    progetto completo

--- progetto.py
+++ progetto.py
@@ -1,5 +1,5 @@
 import random
-
+budget = 1000
 def analisi_carte(mano, tavolo):
     carte = mano + tavolo
     dizionario_carte = dict()
@@ -13,26 +13,44 @@ def analisi_carte(mano, tavolo):
         except KeyError:
             dizionario_carte[carte[i][0]] = 1
     return dizionario_carte
-                
+
+def vittoria(giocatore, avversario, valore_g, valore_a, budget, soldi):
+    if giocatore and avversario:
+        if valore_g>=valore_a:
+            print("HAI VINTO!!!")
+            budget = budget + int(soldi)*2
+            print("il tuo budget è adesso di: " + str(budget))
+        else:
+            print("hai perso")
+            print("il tuo budget è adesso di: " + str(budget))
+    elif giocatore:
+        print("HAI VINTO!!!")
+        budget = budget + int(soldi)*2
+        print("il tuo budget è adesso di: " + str(budget))
+    else:
+        print("hai perso")
+        print("il tuo budget è adesso di: " + str(budget))
+    return budget
+            
 simboli = ["fiori", "picche", "quadri","cuori"]
 valori = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 random_generator = random.Random()
-carte_giocatore = list()
-carte_avversario = list()
-carte_tavolo = list()
-carte_mazzo = [[s, v] for s in simboli for v in valori]
-random_generator.shuffle(carte_mazzo)
-controllo = 0
-budget = 1000
-gioco = 1
-while gioco = 1:
+aumento = 0
+gioco = "1"
+while gioco == "1":
+    carte_mazzo = [[s, v] for s in simboli for v in valori]
+    random_generator.shuffle(carte_mazzo)
+    carte_giocatore = list()
+    carte_avversario = list()
+    carte_tavolo = list()
+    controllo = 0
     while controllo<5:
         if controllo < 2:
             carte_giocatore.append(carte_mazzo.pop())
             carte_avversario.append(carte_mazzo.pop())
         carte_tavolo.append(carte_mazzo.pop())
         controllo += 1
-    print ("ecco le sue carte, ha un budget iniziale di 1000 euro, usali bene: \n \n" + str(carte_giocatore) + "\n \n")
+    print ("ecco le sue carte, ha un budget iniziale di " + str(budget) + " euro, usali bene: \n \n" + str(carte_giocatore) + "\n \n")
     print("CARTE DEL TAVOLO: \n \n " + str(carte_tavolo[0:3]) )
     if int(carte_giocatore [0][1]) >= int(carte_giocatore [1][1]):
         carta_alta_g = int(carte_giocatore[0][1])
@@ -82,19 +100,6 @@ while gioco = 1:
     elif budget == 0:
         print("sei già all in")
     print("\n Signore e signori, showdown: \n \n" + "LE TUE CARTE: \n \n" + str(carte_giocatore) + "\n \nCARTE DEL TAVOLO: \n \n " + str(carte_tavolo) + "\n \n CARTE AVVERSARIO: \n \n " + str(carte_avversario))
-    def vittoria(giocatore, avversario, valore_g, valore_a):
-        if giocatore and avversario:
-            if valore_g>valore_a:
-                print("HAI VINTO!!!")
-                budget = budget + aumento
-            else:
-                print("hai perso")
-        elif giocatore:
-            print("HAI VINTO!!!")
-            budget = budget + aumento
-        else:
-            print("hai perso")
-
     carte_tot_giocatore = analisi_carte(carte_giocatore, carte_tavolo)
     carte_tot_avversario = analisi_carte(carte_avversario, carte_tavolo)
     coppia_g = False
@@ -105,8 +110,8 @@ while gioco = 1:
     poker_g = False
     full_g = False
     scala_colore_g = False
-    valore_coppia_g = []
-    valore_tris_g = []
+    valore_coppia_g = [0]
+    valore_tris_g = [0]
     valore_poker_g = 0
     inizio_scala_g = 0
     simbolo_scala_g = "Nessuno"
@@ -124,7 +129,7 @@ while gioco = 1:
                 coppia_g = True
                 valore_coppia_g.append(int(i))
                 if len(valore_coppia_g) > 2:
-                    del min(valore_coppia_g) 
+                    del valore_coppia_g[min(valore_coppia_g)] 
             elif carte_tot_giocatore[i] == 3:
                 tris_g = True
                 valore_tris_g.append(int(i))
@@ -147,8 +152,10 @@ while gioco = 1:
     colore_a = False
     scala_a = False
     poker_a= False
-    valore_coppia_a = []
-    valore_tris_a = []
+    scala_colore_a = False
+    full_a = False
+    valore_coppia_a = [0]
+    valore_tris_a = [0]
     valore_poker_a = 0
     inizio_scala_a = 0
     simbolo_scala_a = "Nessuno"
@@ -166,7 +173,7 @@ while gioco = 1:
                 coppia_a = True
                 valore_coppia_a.append(int(i))
                 if len(valore_coppia_a) > 2:
-                    del min(valore_coppia_a) 
+                    del valore_coppia_a[min(valore_coppia_a)] 
             elif carte_tot_avversario[i] == 3:
                 tris_a = True
                 valore_tris_a.append(int(i))
@@ -185,46 +192,59 @@ while gioco = 1:
         scala_colore_a = True
 
     if scala_colore_g or scala_colore_a:
-        vittoria(scala_colore_g, scala_colore_a, inizio_scala_g, inizio_scala_a)
+        budget = vittoria(scala_colore_g, scala_colore_a, inizio_scala_g, inizio_scala_a, budget, tavolo)
     elif poker_g or poker_a:
-        vittoria(poker_g, poker_a, valore_poker_g, valore_poker_a)
+        budget = vittoria(poker_g, poker_a, valore_poker_g, valore_poker_a, budget, tavolo)
     elif full_g or full_a:
         if max(valore_tris_g) != max(valore_tris_a):
-            vittoria(full_g, full_a, max(valore_tris_g), max(valore_tris_a))
+            budget = vittoria(full_g, full_a, max(valore_tris_g), max(valore_tris_a), budget, tavolo)
         elif max(valore_coppia_g) != max(valore_tris_a):
-            vittoria(full_g, full_a, max(valore_coppia_g), max(valore_coppia_a))
+            budget = vittoria(full_g, full_a, max(valore_coppia_g), max(valore_coppia_a), budget, tavolo)
         elif carta_alta_a>carta_alta_g:
             print("hai perso")
+            print("il tuo budget è adesso di: " + str(budget))
         else:
             print("HAI VINTO!!!")
-            budget = budget + aumento
+            budget = budget + int(tavolo)*2
+            print("il tuo budget è adesso di: " + str(budget))
     elif colore_g or colore_a:
-        vittoria(colore_g, colore_a, carta_alta_g, carta_alta_a)
+        budget = vittoria(colore_g, colore_a, carta_alta_g, carta_alta_a, budget, tavolo)
     elif scala_g or scala_a:
-        vittoria(scala_g, scala_a, inizio_scala_g, inizio_scala_a)
+        budget = vittoria(scala_g, scala_a, inizio_scala_g, inizio_scala_a, budget, tavolo)
     elif doppia_coppia_g or doppia_coppia_a:
-        if max(valore_coppia_g) != max(valore_coppia_a):
-            vittoria(doppia_coppia_g, doppia_coppia_a, max(valore_coppia_g), max(valore_coppia_a))
-        elif min(valore_coppia_g) != min(valore_coppia_a):
-            vittoria(doppia_coppia_g, doppia_coppia_a, min(valore_coppia_g), min(valore_coppia_a))
+        if len(valore_coppia_g) == len(valore_coppia_a):
+            if max(valore_coppia_g) != max(valore_coppia_a):
+                budget = vittoria(doppia_coppia_g, doppia_coppia_a, max(valore_coppia_g), max(valore_coppia_a), budget, tavolo)
+            elif min(valore_coppia_g) != min(valore_coppia_a):
+                budget = vittoria(doppia_coppia_g, doppia_coppia_a, min(valore_coppia_g), min(valore_coppia_a), budget, tavolo)
+            else:
+                budget = vittoria(doppia_coppia_g, doppia_coppia_a, carta_alta_g, carta_alta_a, budget, tavolo)
         else:
-            vittoria(doppia_coppia_g, doppia_coppia_a, carta_alta_g, carta_alta_a)
+            if len(valore_coppia_a) == 1:
+                print("HAI VINTO!!!")
+                budget = budget +int(tavolo)*2
+                print("il tuo budget è adesso di: " + str(budget))
+                budget = budget + int(tavolo)*2
+            elif len(valore_coppia_g) ==1:
+                print("hai perso")
+                print("il tuo budget è adesso di: " + str(budget))
     elif tris_g or tris_a:
         if max(valore_tris_g) != max(valore_tris_a):
-            vittoria(tris_g, tris_a, max(valore_tris_g), max(valore_tris_a))
+            budget = vittoria(tris_g, tris_a, max(valore_tris_g), max(valore_tris_a), budget, tavolo)
         else:
-            vittoria(tris_g, tris_a, carta_alta_g, carta_alta_a)
+            budget = vittoria(tris_g, tris_a, carta_alta_g, carta_alta_a, budget, tavolo)
     elif coppia_g or coppia_a:
         if max(valore_coppia_g) != max(valore_coppia_a):
-            vittoria(coppia_g, coppia_a, max(valore_coppia_g), max(valore_coppia_a))
+            budget = vittoria(coppia_g, coppia_a, max(valore_coppia_g), max(valore_coppia_a), budget, tavolo)
         else:
-            vittoria(coppia_g, coppia_a, carta_alta_g, carta_alta_a)
+            budget = vittoria(coppia_g, coppia_a, carta_alta_g, carta_alta_a, budget, tavolo)
     else:
         if carta_alta_g >= carta_alta_a:
             print("HAI VINTO!!!")
-            budget = budget + aumento
+            budget = budget + int(tavolo)*2
+            print("il tuo budget è adesso di: " + str(budget))
         else:
             print("hai perso")
-    print("il tuo budget è adesso di: " + str(budget))
-    gioco = input("vuoi fare un'altra mano (0 = no, altro tasto = sì)? ")
+            print("il tuo budget è adesso di: " + str(budget))
+    gioco = input("vuoi fare un'altra mano (1 = sì , altro tasto = no)? ")
     

ffc95cfce77f814b29345ca1197b6d29a9e11f5a
Danicico <danicicots@gmail.com>
2026-06-19 06:54:57 +0000

    controllo programma 3 e 4

--- esercizio_3.py
+++ esercizio_3.py
@@ -1,6 +1,37 @@
-import sys
+#
+# File: esercizio_3.py
+#
+# Author: D. Cicogna
+#
+# Date: 19/06/2026
+#
+# Version: 1.0
+#
+# Description: Partendo dal dizionario annidato rubrica:
+#visualizzate il contenuto del dizionario stampando a schermo delle stringhe formattate che contengano la chiave 
+#ed il valor di ognuno degli elementi(Esempio: ‘Paolino Paperino’, ‘giorno’ 9, ‘mese’ ‘giugno’, …)
+#
+#A partire dalla rubrica, costruire la lista delle età, ordinata in ordine crescente e visualizzate i nomi in ordine crescente di età
+#
+#Invertire l’ordine della lista precedentemente costruita e visualizzatela
+#
+#Utilizzare la rubrica e scrivere su schermo per OGNI membro della rubrica, il seguente messaggio:
+#'''Car[o/a] [Nome],
+#sei nat[o/a] il [giorno] di [mese] del [anno] e quindi a breve compirai [età] anni.
+#Ti manderemo gli auguri a [mail]'''
+#dove [o/a] deve essere adattato all’attributo [M/F]
+#
+#Utilizzando args passate in input al vostro programma una chiave [giorno, mese, anno, età, sesso, mail] 
+#e visualizzate tutto il contenuto della rubrica (valori) che corrispondono a questa chiave
+#
+#Utilizzando argparse visualizzate la stringa al punto 4 SOLO per il nome fornito come opzione al vostro programma 
+#(esempio: python esercizio_3.py –nome ‘Madoka Ayukawa’ –> esegue punto 4 solo per il nome indicato)
+#
+#Utilizzando argparse introducede delle opzioni al vostro programma per eseguire i punti 1, 2, 3, 4, 5 dell’esercizio 
+#(esempio: python esercizio_3.py –lista_ordinata –> esegue il punto 2 dell’esercizio)
+#
+import sys                              #importo delle librerie per poter inserire elementi dalla barra del terminale
 import argparse
-
 rubrica = {
   'Paolino Paperino': {'giorno': 9,
                       'mese': 'giugno',
@@ -19,6 +50,7 @@ rubrica = {
 }
 ciclo = True
 def formattazione_frasi():
+    """stampo una frase per ogni personaggio nella lista, facendo differenza fra uomo e donna"""
     lista_persone = []
     for i in rubrica:
         giorno = rubrica [i]['giorno']
@@ -35,6 +67,7 @@ def formattazione_frasi():
     return lista_persone
 
 def lista_età():
+    """creo una lista ordinata in base all'età"""
     rubrica_ordinata = dict(sorted(rubrica.items(), key = lambda x: x[1]['età']))        #comando usato per mettere in ordine i dizionari secondo un parametro particolare
     lista_nomi_ordinati = []
     for i in rubrica_ordinata:
@@ -42,6 +75,7 @@ def lista_età():
     return lista_nomi_ordinati
 
 def lista_età_contrario():
+    """creo la stessa lista di prima ma capovolta"""
     rubrica_ordinata = dict(sorted(rubrica.items(), key = lambda x: x[1]['età'], reverse=True))  #comando usato per mettere in ordine i dizionari secondo un parametro particolare al contrario
     lista_nomi_ordinati = []
     for i in rubrica_ordinata:
@@ -49,7 +83,7 @@ def lista_età_contrario():
     return lista_nomi_ordinati
 
 def messaggio_personaggio(controllo):
-
+    """creo un messaggio personalizzato per un singolo personaggio"""
     giorno = rubrica [controllo]['giorno']
     mese = rubrica [controllo]['mese']
     anno = rubrica [controllo]['anno']
@@ -60,28 +94,36 @@ def messaggio_personaggio(controllo):
         carattere = 'o'
     else:
         carattere = 'a'
-    out = f'Car{carattere} {controllo},\n sei nat{carattere} il {giorno} di {mese} del {anno} e quindi a breve compirai {età} anni. \n Ti manderemo gli auguri a {mail}'
+    out = (f'Car{carattere} {controllo},\n sei nat{carattere} il {giorno} di {mese} del {anno}'
+           f'e quindi a breve compirai {età} anni. \n Ti manderemo gli auguri a {mail}')
     return out 
 
 def dato_cercato_sys ():
-    dizionario_persone = {}
-    if sys.argv [1] == 'giorno' or sys.argv [1]== 'mese' or sys.argv [1]== 'anno' or sys.argv [1]== 'età' or sys.argv [1]== 'sesso' or sys.argv [1]== 'mail':
-        for i in rubrica:
-            dizionario_persone [i] = rubrica [i] [sys.argv [1]]
-    else:
-        dizionario_persone['errore'] = 'valore non valido, mi dispiace'
+    try:
+        """cerco un personaggio in base ad un dato inserito via sys"""
+        dizionario_persone = {}
+        if sys.argv [1] == 'giorno' or sys.argv [1]== 'mese' or sys.argv [1]== 'anno' or \
+        sys.argv [1]== 'età' or sys.argv [1] == 'sesso' or sys.argv [1] == 'mail':
+            for i in rubrica:
+                dizionario_persone [i] = rubrica [i] [sys.argv [1]]
+        else:
+            dizionario_persone['errore'] = 'valore non valido, mi dispiace'
+    except IndexError:
+        dizionario_persone ['errore'] = "non hai inserito nulla"
     return dizionario_persone
 
-def dato_cercato_argv (parola):
+def dato_cercato_argv (chiave):
+    """faccio lo stesso ragionamento di prima, ma con argv"""
     dizionario_persone = {}
-    if  chiave == 'giorno' or chiave == 'mese' or chiave == 'anno' or chiave== 'età' or chiave == 'sesso' or chiave== 'mail':
+    if  chiave == 'giorno' or chiave == 'mese' or chiave == 'anno' or chiave== 'età' \
+    or chiave == 'sesso' or chiave== 'mail':
         for i in rubrica:
             dizionario_persone [i] = rubrica [i] [chiave]
     else:
         dizionario_persone['errore'] = 'valore non valido, mi dispiace'
     return dizionario_persone
 
-
+# scrivo il messaggio per l'utente del programma in caso si usi argvparse
 parser = argparse.ArgumentParser()
 parser.add_argument ('-cntrl', '--controllo', help='codice di controllo: 1: presentazione di tutti i personaggi; 2: lista in base alla età; 3: listà in base alla età al contrario; 4: messaggio del personaggio; 5: cercare un dato preciso')
 parser.add_argument ('-n', '--nome', help='nome del personaggio da mettere fra Paolino, Ron, Ramona, Madoka (se controllo = 4)')
@@ -96,8 +138,11 @@ elif controllo == '2':
 elif controllo == '3':
     print(lista_età_contrario())
 elif controllo == '4':
-    controllo_nome = args.nome + ' ' + args.cognome
-    print(messaggio_personaggio(controllo_nome))
+    try:
+        controllo_nome = args.nome + ' ' + args.cognome
+        print(messaggio_personaggio(controllo_nome))
+    except TypeError:
+        print("non hai inserito tutti i dati richiesti")
 elif controllo == '5':
     chiave = args.chiave
     print(dato_cercato_argv(chiave))
--- esercizio_4.py
+++ esercizio_4.py
@@ -1,4 +1,23 @@
-import sys
+#
+# File: esercizio_4.py
+#
+# Author: D.Cicogna
+#
+# Date: 19/06/2026
+#
+# Version: 1.0
+#
+# Description: Partendo dall’esercizio 3, aggiungete una opzione al programma per generare un file di testo rubrica.txt 
+#contenente tutti gli elmenti della rubrica, uno per linea, con tutte le informazioni separate da virgole. Ad esempio:
+#Paolino Paperino, 9, giugno, 1934, 89, M, paolino.paperin0@disney.org
+#Ron Weasley, 1, marzo, 1980, 43, M, ron_weasley80@hogwards.uk
+#ecc...
+#
+#Create un file JSON che contiene la rubrica con la stessa struttura del dizionario interno al programma
+#
+#Leggete la rubrica salvata in un file formato JSON e visualizzate tutto il contenuto
+#
+import sys                              #importo delle librerie per poter inserire elementi dalla barra del terminale
 import argparse
 import json
 
@@ -19,7 +38,9 @@ rubrica = {
 'Madoka Ayukawa': {'giorno': 25, 'mese': 'maggio', 'anno': 1969, 'età': 57, 'sesso': 'F', 'mail': 'madoka_sax@asahi_net.jp'}
 }
 ciclo = True
+
 def formattazione_frasi():
+    """stampo una frase per ogni personaggio nella lista, facendo differenza fra uomo e donna"""
     lista_persone = []
     for i in rubrica:
         giorno = rubrica [i]['giorno']
@@ -36,6 +57,7 @@ def formattazione_frasi():
     return lista_persone
 
 def lista_età():
+    """creo una lista ordinata in base all'età"""
     rubrica_ordinata = dict(sorted(rubrica.items(), key = lambda x: x[1]['età']))        #comando usato per mettere in ordine i dizionari secondo un parametro particolare
     lista_nomi_ordinati = []
     for i in rubrica_ordinata:
@@ -43,6 +65,7 @@ def lista_età():
     return lista_nomi_ordinati
 
 def lista_età_contrario():
+    """creo la stessa lista di prima ma capovolta"""
     rubrica_ordinata = dict(sorted(rubrica.items(), key = lambda x: x[1]['età'], reverse=True))  #comando usato per mettere in ordine i dizionari secondo un parametro particolare al contrario
     lista_nomi_ordinati = []
     for i in rubrica_ordinata:
@@ -50,7 +73,7 @@ def lista_età_contrario():
     return lista_nomi_ordinati
 
 def messaggio_personaggio(controllo):
-
+    """creo un messaggio personalizzato per un singolo personaggio"""
     giorno = rubrica [controllo]['giorno']
     mese = rubrica [controllo]['mese']
     anno = rubrica [controllo]['anno']
@@ -61,28 +84,36 @@ def messaggio_personaggio(controllo):
         carattere = 'o'
     else:
         carattere = 'a'
-    out = f'Car{carattere} {controllo},\n sei nat{carattere} il {giorno} di {mese} del {anno} e quindi a breve compirai {età} anni. \n Ti manderemo gli auguri a {mail}'
+    out = (f'Car{carattere} {controllo},\n sei nat{carattere} il {giorno} di {mese} del {anno}'
+           f'e quindi a breve compirai {età} anni. \n Ti manderemo gli auguri a {mail}')    
     return out 
 
 def dato_cercato_sys ():
-    dizionario_persone = {}
-    if sys.argv [1] == 'giorno' or sys.argv [1]== 'mese' or sys.argv [1]== 'anno' or sys.argv [1]== 'età' or sys.argv [1]== 'sesso' or sys.argv [1]== 'mail':
-        for i in rubrica:
-            dizionario_persone [i] = rubrica [i] [sys.argv [1]]
-    else:
-        dizionario_persone['errore'] = 'valore non valido, mi dispiace'
+    try:
+        """cerco un personaggio in base ad un dato inserito via sys"""
+        dizionario_persone = {}
+        if sys.argv [1] == 'giorno' or sys.argv [1]== 'mese' or sys.argv [1]== 'anno' or \
+        sys.argv [1]== 'età' or sys.argv [1] == 'sesso' or sys.argv [1] == 'mail':
+            for i in rubrica:
+                dizionario_persone [i] = rubrica [i] [sys.argv [1]]
+        else:
+            dizionario_persone['errore'] = 'valore non valido, mi dispiace'
+    except IndexError:
+        dizionario_persone ['errore'] = "non hai inserito nulla"
     return dizionario_persone
 
-def dato_cercato_argv (parola):
+def dato_cercato_argv (chiave):
+    """faccio lo stesso ragionamento di prima, ma con argv"""
     dizionario_persone = {}
-    if  chiave == 'giorno' or chiave == 'mese' or chiave == 'anno' or chiave== 'età' or chiave == 'sesso' or chiave== 'mail':
+    if  chiave == 'giorno' or chiave == 'mese' or chiave == 'anno' or chiave== 'età' \
+    or chiave == 'sesso' or chiave== 'mail':
         for i in rubrica:
             dizionario_persone [i] = rubrica [i] [chiave]
     else:
         dizionario_persone['errore'] = 'valore non valido, mi dispiace'
     return dizionario_persone
 
-
+# scrivo il messaggio per l'utente del programma in caso si usi argvparse
 parser = argparse.ArgumentParser()
 parser.add_argument ('-cntrl', '--controllo', help='codice di controllo: 1: presentazione di tutti i personaggi; 2: lista in base alla età; 3: listà in base alla età al contrario; 4: messaggio del personaggio; 5: cercare un dato preciso: 6: crea un file di testo')
 parser.add_argument ('-n', '--nome', help='nome del personaggio da mettere fra Paolino, Ron, Ramona, Madoka (se controllo = 4)')
@@ -97,20 +128,25 @@ elif controllo == '2':
 elif controllo == '3':
     print(lista_età_contrario())
 elif controllo == '4':
-    controllo_nome = args.nome + ' ' + args.cognome
-    print(messaggio_personaggio(controllo_nome))
+    try:
+        controllo_nome = args.nome + ' ' + args.cognome
+        print(messaggio_personaggio(controllo_nome))
+    except TypeError:
+        print("non hai inserito tutti i dati richiesti")
 elif controllo == '5':
     chiave = args.chiave
     print(dato_cercato_argv(chiave))
 elif controllo == '6':
-    righe_nomi = open('righe_nomi.txt', 'w')
+    righe_nomi = open('righe_nomi.txt', 'w')                #apro un file di testo di tipo txt
     for i in rubrica:
-        testo = '' + i + ' , ' + str(rubrica [i]['giorno']) + ' , ' + str(rubrica [i]['mese']) + ' , ' + str(rubrica [i]['anno']) + ' , ' + str(rubrica [i] ['età']) + ' , ' + str(rubrica [i]['sesso']) + ' , '  + str(rubrica[i]['mail']) + '\n'
+        testo = '' + i + ' , ' + str(rubrica [i]['giorno']) + ' , ' + str(rubrica [i]['mese']) + ' , ' + str(rubrica [i]['anno']) + ' , ' \
+                + str(rubrica [i] ['età']) + ' , ' + str(rubrica [i]['sesso']) + ' , '  + str(rubrica[i]['mail']) + '\n'
         righe_nomi.write(testo)
     righe_nomi.close()
 else:
     print('comando sbagliato, mi dispiace')
 
+#apro e prima scrivo e poi leggo un file json
 with open ("dizionario_file.json", "w") as write_file:
     json.dump(rubrica, write_file, indent = 4)
 with open ('dizionario_file.json', "r") as in_file:
